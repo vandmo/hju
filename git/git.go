2862178a45a1fca14b1b7b97ccbd91a005a600e9
Mikael Vandmo <mikael.vandmo@wirelesscar.com>
2021-09-20 10:26:29 +0200

    Extract git/run.go

--- git/git.go
+++ git/git.go
@@ -4,41 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
-
-	"github.com/cli/safeexec"
 )
-
-func command(arg ...string) (*exec.Cmd, error) {
-	gitBin, err := safeexec.LookPath("git")
-	if err != nil {
-		return nil, err
-	}
-	return exec.Command(gitBin, arg...), nil
-}
-
-func run(arg ...string) error {
-	cmd, err := command(arg...)
-	if err != nil {
-		return err
-	}
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
-}
-
-func runC(folder string, arg0 string, arg1 string) (string, error) {
-	cmd, err := command("-C", folder, arg0, arg1)
-	if err != nil {
-		return "", err
-	}
-	out, cmdErr := cmd.Output()
-	if cmdErr != nil {
-		return "", cmdErr
-	}
-	return string(out), nil
-}
 
 func SymbolicRef(folder string, symbol string) (string, error) {
 	return runC(folder, "symbolic-ref", symbol)
